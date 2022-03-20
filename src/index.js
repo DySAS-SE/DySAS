a0dd08bae9e5b416a4fb694ca2e127b61b0851fd
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-03-20 19:53:05 +0530

    Add header to the login page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import {
+  Toolbar,
+  AppBar,
+  Button,
+  Box,
+  TextField,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function NavBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="h1"
+            color="inherit"
+            sx={{ flexGrow: 1 }}
+          >
+            DySAS
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 function LoginForm() {
   return (
-    <form>
+    <form margin="auto">
       <TextField id="username" label="Username" margin="normal" fullWidth />
       <TextField id="password" label="Password" margin="normal" fullWidth />
-      <Button type="submit" variant="contained" color="primary">
-        Login
-      </Button>
+      <Box textAlign="center">
+        <Button type="submit" variant="contained" color="primary" size="large">
+          Login
+        </Button>
+      </Box>
     </form>
   );
 }
 
 function App(props) {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={3}>
-        <LoginForm />
+    <Box>
+      <NavBar />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item xs={3}>
+          <LoginForm />
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 }
 
-ReactDOM.render(<App message="Goodbye" />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
