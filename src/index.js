e6987332bad408404e16ed853dc2cd2581d622c1
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-03-21 13:05:45 +0530

    Add UI for the courses page

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import {
   Button,
   Box,
   TextField,
-  Grid,
   IconButton,
   Typography,
+  Container,
+  Card,
+  CardContent,
+  CardActionArea,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+
+let loggedIn = true;
 
 function NavBar() {
   return (
@@ -43,11 +48,20 @@ function NavBar() {
 function LoginForm() {
   return (
     // TODO: center align the form (vertically)
-    <Box sx={{ mx: "auto", mt: "5em", padding: "1em", maxWidth: "30em" }}>
+    <Box sx={{ mx: "auto", maxWidth: "30em" }}>
+      <Typography textAlign="center" variant="h2" component="h2" gutterBottom>
+        Login
+      </Typography>
       <form className="loginForm">
         <TextField id="username" label="Username" margin="normal" fullWidth />
-        <TextField id="password" label="Password" margin="normal" fullWidth />
-        <Box textAlign="center" sx={{ padding: 2 }}>
+        <TextField
+          id="password"
+          label="Password"
+          margin="normal"
+          fullWidth
+          type="password"
+        />
+        <Box textAlign="right" sx={{ mt: "1em" }}>
           <Button
             type="submit"
             variant="contained"
@@ -62,18 +76,50 @@ function LoginForm() {
   );
 }
 
-function App(props) {
+function CourseItem(props) {
+  return (
+    <Card sx={{ width: "100%" }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography variant="h4" component="h3" gutterBottom>
+            {props.course.name}
+          </Typography>
+          <Typography variant="body1">{props.course.description}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+function Dashboard() {
   return (
     <Box>
-      <NavBar />
-      <LoginForm />
+      <Typography variant="h2" component="h2" gutterBottom>
+        Courses
+      </Typography>
+      <CourseItem
+        course={{ name: "Course 1", description: "This is a course" }}
+      />
     </Box>
   );
 }
 
+function MainContent() {
+  if (loggedIn) {
+    return <Dashboard />;
+  } else {
+    return <LoginForm />;
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Box>
+      <NavBar />
+      <Container sx={{ maxWidth: "100em", my: "5em", padding: "1em" }}>
+        <MainContent />
+      </Container>
+    </Box>
   </React.StrictMode>,
   document.getElementById("root")
 );
