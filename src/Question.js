d4dddad02d4a3f7cc534362a465322b6b06d22d9
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-03-27 21:12:39 +0530

    Improve Question page

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,15 @@
-import { Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import React from "react";
 
 function Question() {
   return (
     <Box>
-      <Typography variant="h3"> CS330 | Quiz 1</Typography>
-      <card>
-        <text>
+      <Typography variant="h3">CS330 | Quiz 1</Typography>
+      <Card>
+        <Typography>
           What is the probability that the result of a coin toss is heads?
-        </text>
-      </card>
+        </Typography>
+      </Card>
     </Box>
   );
 }
