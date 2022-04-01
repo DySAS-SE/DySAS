852f73c76103bb40e6c53731708835bb70f11559
WhyBangad <byatin2005@gmail.com>
2022-04-01 17:20:32 +0530

    Working question page

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Template from "./Template";
-import Login from "./Login";
-import DashBoard from "./Dashboard";
-import CoursePage from "./CoursePage";
+import Template from "./components/Template";
+import Login from "./components/Login";
+import DashBoard from "./components/Dashboard";
+import CoursePage from "./components/CoursePage";
+import Question from "./components/Question";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.render(
           <Route path="login" element={<Login />} />
           <Route path="dashboard" element={<DashBoard />} />
           <Route path="coursepage" element={<CoursePage />} />
+          <Route path="question" element={<Question />} />
         </Route>
         {/* No match route */}
         <Route
---
+++ frontend/src/components/Question.js
@@ -0,0 +1,114 @@
+import {
+  Box,
+  Card,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+  Button,
+} from "@mui/material";
+import React from "react";
+import "../css/Question.css";
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+    };
+    // necessary to make this work
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log("change!");
+    const value = event.target.value;
+    const nextState = Object.assign({}, this.state);
+    nextState[value] = true;
+    this.setState(nextState);
+  }
+
+  handleSubmit() {}
+
+  render() {
+    const clearState = {
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+    };
+    const options = this.state;
+    return (
+      <Box>
+        <Typography variant="h3">CS330 | Quiz 1</Typography>
+        <Card sx={{ mt: "2em", padding: "2em" }}>
+          <Typography variant="h6">
+            Q1) What is the probability that the result of a coin toss is heads?
+          </Typography>
+          <Box sx={{ mt: "1em" }}>
+            <FormControl>
+              <FormLabel id="choices">Answer:</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="a"
+                  control={<Radio />}
+                  checked={options.a}
+                  label="0.5"
+                  onChange={this.handleChange}
+                ></FormControlLabel>
+                <FormControlLabel
+                  value="b"
+                  control={<Radio />}
+                  checked={options.b}
+                  label="0.25"
+                  onChange={this.handleChange}
+                ></FormControlLabel>
+                <FormControlLabel
+                  value="c"
+                  control={<Radio />}
+                  checked={options.c}
+                  label="0.75"
+                  onChange={this.handleChange}
+                ></FormControlLabel>
+                <FormControlLabel
+                  value="d"
+                  control={<Radio />}
+                  checked={options.d}
+                  label="1"
+                  onChange={this.handleChange}
+                ></FormControlLabel>
+              </RadioGroup>
+              <Box sx={{ padding: "0.5em", gap: "0.5em" }}>
+                <Button
+                  sx={{ padding: "0.5em", mt: "0.25em" }}
+                  type="clear"
+                  variant="outlined"
+                  onClick={() => this.setState(clearState)}
+                  className="action-btns"
+                >
+                  Clear choice
+                </Button>
+                <Button
+                  sx={{ padding: "0.5em", mt: "0.25em" }}
+                  type="submit"
+                  variant="outlined"
+                  onClick={() => this.handleSubmit()}
+                  className="action-btns"
+                >
+                  Next
+                </Button>
+              </Box>
+            </FormControl>
+          </Box>
+        </Card>
+      </Box>
+    );
+  }
+}
+
+export default Question;
